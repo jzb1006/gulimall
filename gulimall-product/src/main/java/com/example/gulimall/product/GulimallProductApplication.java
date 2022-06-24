192d236e4e98cd279f87f4bd8c06d43601f37d52
jzb1006 <1129990545@qqq.com>
2022-06-24 22:33:54 +0800

    添加商品网关配置

--- gulimall-product/src/main/java/com/example/gulimall/product/GulimallProductApplication.java
+++ gulimall-product/src/main/java/com/example/gulimall/product/GulimallProductApplication.java
@@ -3,12 +3,14 @@ package com.example.gulimall.product;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author jiang
  */
 @MapperScan("com.example.gulimall.product.dao")
 @SpringBootApplication
+@EnableDiscoveryClient
 public class GulimallProductApplication {
 
     public static void main(String[] args) {
