cf6f161ea32b08a693453cde05320e2c66197e24
zhibinjiang <zhibinjiang@chatly.com>
2022-06-28 14:21:16 +0800

    增加elasticsearch 搜索模块

---
+++ gulimall-search/src/main/java/com/example/gulimall/search/config/GulimallElasticSearchConfig.java
@@ -0,0 +1,33 @@
+package com.example.gulimall.search.config;
+
+import co.elastic.clients.elasticsearch.ElasticsearchClient;
+import co.elastic.clients.json.jackson.JacksonJsonpMapper;
+import co.elastic.clients.transport.ElasticsearchTransport;
+import co.elastic.clients.transport.rest_client.RestClientTransport;
+import org.apache.http.HttpHost;
+import org.elasticsearch.client.RestClient;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+/**
+ * @author ZhibinJiang on 2022/6/28
+ */
+@Configuration
+public class GulimallElasticSearchConfig {
+
+
+    @Bean
+    public ElasticsearchClient esClient(){
+        // Create the low-level client
+        RestClient restClient = RestClient.builder(
+                new HttpHost("36.139.164.245", 9200)).build();
+
+// Create the transport with a Jackson mapper
+        ElasticsearchTransport transport = new RestClientTransport(
+                restClient, new JacksonJsonpMapper());
+
+// And create the API client
+
+        return new ElasticsearchClient(transport);
+    }
+}
