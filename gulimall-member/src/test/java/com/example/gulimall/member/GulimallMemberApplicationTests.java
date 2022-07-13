b47c865d2346207037a3d2f715706c15c4e66fa6
jiangzhibin <jiangzhibin1994@gmail.com>
2022-07-13 17:46:50 +0800

    测试密码加密存储工具

--- gulimall-member/src/test/java/com/example/gulimall/member/GulimallMemberApplicationTests.java
+++ gulimall-member/src/test/java/com/example/gulimall/member/GulimallMemberApplicationTests.java
@@ -2,12 +2,22 @@ package com.example.gulimall.member;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootTest
 class GulimallMemberApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void testPasswordBCrypt() {
+        BCryptPasswordEncoder encoder = new BCryptPasswordEncoder();
+        String password = "123456";
+        String encode = encoder.encode(password);
+        System.out.println(encode);
+        System.out.println(encoder.matches(password,"$2a$10$t8g6Ist9RIHzhdGucWEk1uqQQcy0f16FPuY31ff0L9Gy945fh7jg2"));
     }
 
 }
